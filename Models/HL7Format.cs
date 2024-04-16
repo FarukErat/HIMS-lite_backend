cf6f5d9e9bc3dad7e3279c27488e4270252beb9a
Faruk ERAT <farukerat1327@gmail.com>
2024-04-16 14:22:25 +0300

    simplified model classes

--- Models/HL7Format.cs
+++ Models/HL7Format.cs
@@ -2,40 +2,20 @@ namespace HL7.Models;
 
 public class Component
 {
-    public List<string> Subcomponents { get; set; }
-
-    public Component()
-    {
-        Subcomponents = [];
-    }
+    public List<string> Subcomponents { get; set; } = [];
 }
 
 public class Field
 {
-    public List<Component> Components { get; set; }
-
-    public Field()
-    {
-        Components = [];
-    }
+    public List<Component> Components { get; set; } = [];
 }
 
 public class Segment
 {
-    public List<Field> Fields { get; set; }
-
-    public Segment()
-    {
-        Fields = [];
-    }
+    public List<Field> Fields { get; set; } = [];
 }
 
 public class Message
 {
-    public List<Segment> Segments { get; set; }
-
-    public Message()
-    {
-        Segments = [];
-    }
+    public List<Segment> Segments { get; set; } = [];
 }
